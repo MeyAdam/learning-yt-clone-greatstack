939ecef1228389afe11ef712538ce653c3a0173e
MeyAdam <meyadam11@gmail.com>
2024-12-25 23:45:20 +0800

    Navbar UI

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import iconProfile from "../../assets/jack.png";
+import logo from "../../assets/logo.png";
+import iconMenu from "../../assets/menu.png";
+import iconMore from "../../assets/more.png";
+import iconNotification from "../../assets/notification.png";
+import iconSearch from "../../assets/search.png";
+import iconUpload from "../../assets/upload.png";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="flex-div">
+      <div className="nav-left flex-div">
+        <img src={iconMenu} class="menu-icon" />
+        <img src={logo} class="logo" />
+      </div>
 
-export default Navbar
+      <div class="nav-middle flex-div">
+        <div class="search-box flex-div">
+          <input type="text" placeholder="Search" />
+          <img src={iconSearch} alt="" />
+        </div>
+      </div>
+
+      <div class="nav-right flex-div">
+        <img src={iconUpload} alt="" />
+        <img src={iconMore} alt="" />
+        <img src={iconNotification} alt="" />
+        <img src={iconProfile} alt="" class="user-icon" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
